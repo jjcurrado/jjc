fbccab44810c67d7fa9415b7bb8acd9703a8b5ac
John Currado <jjcurrado@gmail.com>
2023-01-31 13:58:29 -0500

    taking proj on the go

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Block from './Block';
 
+var entries = {
+  "name": "John Currado",
+  "email": "jjcurrado@gmail.com",
+}
 function App() {
   return (
     <div className='App' >
-      <Block></Block>
+      <Block content={entries} /> 
     </div>
   );
 }
--- src/Block.js
+++ src/Block.js
@@ -2,21 +2,31 @@ import './Block.css'
 import React, { useState } from 'react';
 
 
-function Block() {
+function Block(props) {
     const [isCollapsed, setCollapsed] = useState(true);
     
-    function handleExpand()
-    {
+    function handleExpand() {
         setCollapsed(!isCollapsed)
     }
-    if(isCollapsed)
+
+    if (isCollapsed)
         // {...}
         return <button className='collapsed-block' onClick={handleExpand}>
-            &#123;...&#125; 
+            &#123;...&#125;
         </button>
 
     return (
-        <>Hello World!</>
+        <div className='block-body'>
+            &#123;
+            {
+                Object.keys(props.content).map((key) => (
+                    <div className='key-val'>
+                        <span className='obj-key'>"{key}"</span> : <span className='obj-val'>"{props.content[key]}",</span>
+                    </div>
+                ))
+            }
+            &#125;
+        </div>
     )
 
     
