8a3b4ccc11ae6301c8f066b7cce99feaef1e29a4
Currado <jjcurrado@gmail.com>
2023-02-17 13:41:28 -0500

    fixed alignment, added block collapse

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
 import Block from './Block';
 
+
+//TODO:
+// - fix trailing comma after block
+// - add array character for block
+// - add fade in transition for initial collapsed block
+// - add fade in transition for each item in a block
+
 var entries = {
-  "name": "John Currado",
-  "email": "jjcurrado@gmail.com",
+  "name": "\"John Currado\"",
+  "email": "\"jjcurrado@gmail.com\"",
+  "phone" : "\"3154809991\"",
+  "education": <Block content={
+    {
+      "school": "\"RIT\"",
+      "degree": "\"Bachelor's of Science\"",
+      "major": "\"Computation Mathematics\"",
+      "minor": "\"Computer Science\""
+    }
+  } />
 }
 function App() {
   return (
     <div className='App' >
-      <Block content={entries} /> 
+      <Block size={"fullscreen"} content={entries} /> 
     </div>
   );
 }
--- src/Block.js
+++ src/Block.js
@@ -4,6 +4,12 @@ import React, { useState } from 'react';
 
 function Block(props) {
     const [isCollapsed, setCollapsed] = useState(true);
+
+    if (props.blockChar === "square")
+    {
+
+    }
+
     
     function handleExpand() {
         setCollapsed(!isCollapsed)
@@ -11,21 +17,21 @@ function Block(props) {
 
     if (isCollapsed)
         // {...}
-        return <button className='collapsed-block' onClick={handleExpand}>
+        return <button className={"collapsed-block " + props.size} onClick={handleExpand}>
             &#123;...&#125;
         </button>
 
     return (
         <div className='block-body'>
-            &#123;
+            <button className='block-body bracket' onClick={handleExpand}>&#123;</button>
             {
                 Object.keys(props.content).map((key) => (
                     <div className='key-val'>
-                        <span className='obj-key'>"{key}"</span> : <span className='obj-val'>"{props.content[key]}",</span>
+                        <span className='obj-key'>"{key}"</span> : <span className='obj-val'>{props.content[key]},</span>
                     </div>
                 ))
             }
-            &#125;
+            <button className='block-body bracket'>&#125;</button>
         </div>
     )
 
