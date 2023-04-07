714cd579ad283e3c2f1f82f48305f79cae5ec21b
Currado <jjcurrado@gmail.com>
2023-04-07 11:44:10 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
 import './App.css';
 import Block from './Block';
+import FadeIn from 'react-fade-in';
 
 
-//TODO:
-// - fix trailing comma after block
-// - add array character for block
-// - add fade in transition for initial collapsed block
-// - add fade in transition for each item in a block
 
-var entries = {
-  "name": "\"John Currado\"",
-  "email": "\"jjcurrado@gmail.com\"",
-  "phone" : "\"3154809991\"",
-  "education": <Block content={
-    {
-      "school": "\"RIT\"",
-      "degree": "\"Bachelor's of Science\"",
-      "major": "\"Computation Mathematics\"",
-      "minor": "\"Computer Science\""
-    }
-  } />
-}
+var entries = [
+  { "name": "\"John Currado\"" },
+  { "email": "\"jjcurrado@gmail.com\"" },
+  { "phone": "\"3154809991\"" },
+  {"education": <Block  content={
+    [
+      {
+        "Rochester Institue of Technology": <Block content={
+          [
+            { "degree": "\"Bachelor's of Science\"" },
+            { "major": "\"Computation Mathematics\"" },
+            { "minor": "\"Computer Science\"" },
+            { "immersion" : "\"German\""}
+          ]
+        } />
+      },
+      {
+        "Onondaga Community College": <Block content={
+          [
+            { "degree": "\"Associates's of Science\"" },
+            { "major": "\"Computer Science\"" }
+          ]
+        }/>
+      }
+
+    ]
+    }/>
+  },
+  {
+    "experience": <Block content={
+      [
+        {
+          "icitizen": <Block content={
+            [
+              {"start_date": "\"05-2019\""},
+              {"end_date": "\"01-2022\""},
+              {
+                "description": <div className='text-block'>"icitizen is a political discussion website that implements user
+                  polling to collect opinions. My work for icitizen was mostly pertaining to the Alexa
+                  skill for the website (found <a className="link" href="https://www.amazon.com/icitizen/dp/B08R6R5P9K">here</a>).
+                  The company also had a side project that I worked on creating Alexa skills for municipalities to communicate
+                  alerts and community events.I also did a limited amount of work on the frontend for the website itself.
+                </div>
+              },
+              {"skills": <Block type="array" content={["python", "React", "javascript", "AWS Lambda", "MongoDB", "dynamoDB", "Alexa Skills Kit", ]}/>}
+            ]
+        }/>
+        },
+        {
+          "Camp Walden": <Block content={
+            [
+              {"start_date": "\"06-2014\""},
+              {"end_date": "\"07-2015\""},
+              {
+                "description": <div className='text-block'>"Camp Walden is a sleepaway camp for children up to the age of 15. I spent two 
+                  summers there as a bunk counselor. As a bunk counselor I was responsible for a group of roughly a dozen teenagers,
+                  bringing them to activities, resolving conflicts, and ensuring everyone was enjoying their time at the camp."
+                </div>
+              },
+              {"skills": <Block type="array" content={["team communication", "conflict resolution"]}/>}
+            ]
+        }/>
+        },
+        {
+          "Design and Build by Mark Frateschi": <Block content={
+            [
+              {"start_date": "\"06-2017\""},
+              {"end_date": "\"04-2018\""},
+              {
+                "description": <div className='text-block'>
+                  "Mark Frateschi is a general contractor who's skills cover everything from woodworking to electrical to plumbing. Working one
+                  on one with Mark taught me the value of hard work, since every bit of effort could easily
+                  be seen in the finished product. This job also strongly devloped my ability to create a plan and execute it - dealing
+                  with any hiccups along the way - in order to create exactly what the customer wanted and expected. 
+                </div>
+              },
+              {"skills": <Block type="array" content={["planning", "hard work", "wood working", "electrical"]}/>}
+            ]
+        }/>
+        }
+      ]
+    } />
+  }
+]
 function App() {
   return (
     <div className='App' >
--- src/Block.js
+++ src/Block.js
@@ -1,41 +1,78 @@
 import './Block.css'
 import React, { useState } from 'react';
+import FadeIn from 'react-fade-in';
 
 
 function Block(props) {
     const [isCollapsed, setCollapsed] = useState(true);
+    var open = '{';
+    var close = '}';
+    var blockBracketClass = 'block-body bracket'; 
 
-    if (props.blockChar === "square")
+    if (props.type === "array")
     {
-
+        open = '[';
+        close = ']';
+        blockBracketClass += " array";
     }
 
-    
+    // {...}, [...]
+    var collapsed = open + "..." + close;
+
+    // open and close objects
     function handleExpand() {
         setCollapsed(!isCollapsed)
     }
 
     if (isCollapsed)
-        // {...}
+        //  display {...}, [...]
         return <button className={"collapsed-block " + props.size} onClick={handleExpand}>
-            &#123;...&#125;
+            <FadeIn>{collapsed}</FadeIn>
         </button>
+
+    var delay = 400; //fade in animation time in ms
+    var body;
+
+    if (props.type === "array") {
+        body = props.content.slice(0, props.content.length - 1).map((entry) => (
+            <span className='obj-val'>{entry},</span>
+        ))
+        // last object has no trailing comma (find a prettier way to do this?)
+        var len = props.content.length;
+        var lastObject = props.content[len - 1];
+
+        console.log(blockBracketClass)
+        body.push(
+            <span className='obj-val'>{lastObject}</span>
+        )
+    }
+    else {
+        body = props.content.slice(0, props.content.length - 1).map((entry) => (
+            <div className='key-val'>
+                <span className='obj-key'>"{Object.keys(entry)[0]}"</span> : <span className='obj-val'>{entry[Object.keys(entry)]}</span>,
+            </div>
+        ))
+
+        // last object has no trailing comma (find a prettier way to do this?)
+        var len = props.content.length;
+        var lastObject = props.content[len - 1];
+
+        body.push(
+            <div className='key-val'>
+                <span className='obj-key'>"{Object.keys(lastObject)[0]}"</span> : <span className='obj-val'>{lastObject[Object.keys(lastObject)[0]]}</span>
+            </div>
+        )
+    }
 
     return (
         <div className='block-body'>
-            <button className='block-body bracket' onClick={handleExpand}>&#123;</button>
-            {
-                Object.keys(props.content).map((key) => (
-                    <div className='key-val'>
-                        <span className='obj-key'>"{key}"</span> : <span className='obj-val'>{props.content[key]},</span>
-                    </div>
-                ))
-            }
-            <button className='block-body bracket'>&#125;</button>
+                <button className={blockBracketClass} onClick={handleExpand}>{open}</button>
+                <FadeIn delay={delay} wrapperTag='span' childTag='span' className='fade-wrapper'>
+                    {body}
+                </FadeIn>
+                <button className={blockBracketClass}>{close}</button>
         </div>
-    )
-
-    
+    );
 }
 
 export default Block;
